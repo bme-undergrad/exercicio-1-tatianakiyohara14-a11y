060d1f60a7511a408bbc511c8494ba9d96179183
tatianakiyohara14-a11y <tatianakiyohara14@gmail.com>
2025-09-01 22:14:56 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,14 +1,31 @@
-function t = exercicio1(func,x0)
+function t = exercicio1(func,func_d,x0)
 
 % nao alterar: inicio
-es = 1;
+es = 0.01;
 imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % digite seu codigo aqui
-t = 0; % alterar
+
+t = zeros(20,1)
+t(1) = x0
+erro = zeros(length(t),1)
+
+for ii = 1:length(t)-1
+  ii
+  if ii ~= 1
+    erro(ii) = abs((t(ii)-t(ii-1))/t(ii))
+    if erro(ii) < es
+      break
+    endif
+  endif
+  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii))
+endfor
+
+disp('O tempo para que v seja 1m/s é:')
+t(ii)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
